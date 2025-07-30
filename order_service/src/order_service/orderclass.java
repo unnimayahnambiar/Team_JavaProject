cdf2d5fe6a347116a1e4cd9ca1224fd0a00c0a75
VidhyaDharshinie <vidhyadharshinie475@gmail.com>
2025-07-30 14:14:21 +0530

    this is updated code

--- order_service/src/order_service/orderclass.java
+++ order_service/src/order_service/orderclass.java
@@ -3,7 +3,8 @@ package order_service;
 public class orderclass {
 
 	public static void main(String[] args) {
-		System.out.println("lalalallala");;
+		System.out.println("lalalallala");
+		System.out.println("Its your teammate");
 
 	}
 
