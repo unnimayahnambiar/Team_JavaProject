50fa424408adf1e61efb6da72fcb72bd7f816f65
unnim <unnim@LAPTOP-ENE4MB4N>
2025-07-30 12:01:05 +0530

    Created a class. First share to members.

---
+++ order_service/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module order_service {
+}
---
+++ order_service/src/order_service/orderclass.java
@@ -0,0 +1,10 @@
+package order_service;
+
+public class orderclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
